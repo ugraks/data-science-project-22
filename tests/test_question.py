be2744012172858a1a1aec185b035b4924bf88b9
Dogancan Kınık <dogancankinik@gmail.com>
2025-04-28 09:37:37 +0300

    Update test_question.py

--- tests/test_question.py
+++ tests/test_question.py
@@ -135,7 +135,7 @@ def run_tests():
     url = "https://edugen-backend-487d2168bc6c.herokuapp.com/projectLog/"
     payload = {
         "user_id": 34,
-        "project_id": 2,
+        "project_id": 35,
         "user_score": round(user_score, 2),
         "is_auto": False
     }
